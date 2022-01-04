3bb30be508124d1dec2aca9f54b7c5500b81f4a8
alaqsaka <aqsaabidin15@gmail.com>
2022-01-04 19:13:55 +0700

    feat: creating actions and reducers

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import { Provider } from "react";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import App from "./App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

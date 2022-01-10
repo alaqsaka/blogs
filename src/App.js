0efbad0554b7d1161298488b4306124f2ed451ee
alaqsaka <aqsaabidin15@gmail.com>
2022-01-10 20:19:42 +0700

    create form, fix margin in textfield and button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { getPosts } from "./actions/posts";
 import memories from "./images/memories.png";
 import { useDispatch } from "react-redux";
 import Posts from "./components/Posts/Posts";
@@ -8,6 +9,11 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxidth="lg">
@@ -26,7 +32,7 @@ const App = () => {
         <Container>
           <Grid
             container
-            justify="space-betwwen"
+            justifyContent="space-between"
             alignItems="stretch"
             spacing={3}
           >
